ee777441ad908b7f943f15fd035a57b24e291bad
Karen Danielyan <karendanielyan66@gmail.com>
2025-05-26 03:47:22 +0200

    FEAT: +/internal/tokens/verify to verify JWT validity and identify owner

--- backend/authservice/src/routes/auth/logoutRoutes.ts
+++ backend/authservice/src/routes/auth/logoutRoutes.ts
@@ -34,45 +34,4 @@ export default async function logoutRoutes(app: FastifyInstance) {
 			reply.send({revoked: true} satisfies AuthTypes.LogoutResponse);
 		}
 	);
-	app.post<{
-		Body: AuthTypes.InternalRevokeTokenRequest;
-		Reply: AuthTypes.LogoutResponse | CommonTypes.ApiError;
-	}>(
-		'/internal/tokens/revoke',
-		{
-			onRequest: (req, reply, done) => {
-				if (req.headers['x-cluster-token'] !== process.env.CLUSTER_TOKEN) {
-					return reply.sendError({code: 'UNAUTHORIZED', message: 'Invalid cluster token', statusCode: 401 });
-				}
-				done();
-			},
-			schema: {
-				body: { $ref: 'auth.internalRevokeTokenRequest#' },
-				response: {
-					200: { $ref: 'auth.logoutResponse#' },
-					404: { $ref: 'api.error#' }
-				}
-			}
-		},
-		async (req, reply) => {
-			const { tokenId } = req.body;
-			
-			const success = await app.prisma.refreshToken
-				.update({
-					where: { id: tokenId },
-					data:  {
-						revoked: true,
-						revokedReason: 'FORCED_REVOKE',
-						rotatedAt: new Date()
-					}
-				})
-				.then(() => true)
-				.catch(() => false);
-			
-			if (!success) {
-				throw apiError('TOKEN_NOT_FOUND', 'Token id not found or already revoked', 404);
-			}
-			reply.send({ revoked: true });
-		}
-	);
 }
--- backend/authservice/src/routes/auth/routes.ts
+++ backend/authservice/src/routes/auth/routes.ts
@@ -3,6 +3,7 @@ import loginRoutes from './loginRoutes';
 import logoutRoutes from './logoutRoutes';
 import googleOauthRoutes from "./oauth-google";
 import totpRoutes from './totp';
+import internalTokenRoutes from "./internalRoutes";
 
 
 export default async function authRoutes(app: FastifyInstance) {
@@ -10,4 +11,5 @@ export default async function authRoutes(app: FastifyInstance) {
 	await logoutRoutes(app);
 	await googleOauthRoutes(app);
 	await totpRoutes(app);
+	await internalTokenRoutes(app);
 }
