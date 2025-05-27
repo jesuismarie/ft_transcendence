60a56407b07b9d2fb45298fc857332aee128c0e6
almelikyan <almelikyan@ozon.ru>
2025-05-27 12:29:52 +0400

    FEAT: add gamestats handler

--- backend/game-service/src/index.ts
+++ backend/game-service/src/index.ts
@@ -4,6 +4,7 @@ import initDb from "./db/index";
 import matchInvitationRoutes from "./routes/matchInvitation/routes";
 import matchRoutes from "./routes/match/routes";
 import tournamentRoutes from "./routes/tournament/routes";
+import internalRoutes from "./routes/internal/routes";
 
 const app = Fastify({ logger: true });
 
@@ -12,11 +13,11 @@ app.register(initDb);
 
 // Регистрируем маршруты
 app.register(matchInvitationRoutes);
-app.register(matchRoutes)
-app.register(tournamentRoutes)
+app.register(matchRoutes);
+app.register(tournamentRoutes);
+app.register(internalRoutes);
 
-
-app.listen({ port: 3000 }, (err) => {
+app.listen({ port: 3002 }, (err) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
