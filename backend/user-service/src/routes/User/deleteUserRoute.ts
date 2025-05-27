5f9bb2ee8c7d4f63e87c19f37c01f82a247c5469
Karen Danielyan <karendanielyan66@gmail.com>
2025-05-27 22:12:23 +0200

    FIX: deleteUserRoute

--- backend/user-service/src/routes/User/deleteUserRoute.ts
+++ backend/user-service/src/routes/User/deleteUserRoute.ts
@@ -3,10 +3,14 @@ import { UserRepo } from "../../repositories/userRepo";
 
 
 export default async function deleteUserRoute(app: FastifyInstance, userRepo: UserRepo) {
-	app.delete('/users/:id', async (req, reply: FastifyReply) => {
-		const id = Number((req.params as any).id);
-		const deleted = userRepo.delete(id);
-		if (!deleted) return reply.sendError({statusCode: 404, message: 'User not found'});
-		reply.code(204).send();
-	});
+	app.delete<{Params: {id: string}}>(
+		'/users/:id',
+		async (req, reply: FastifyReply) => {
+			const id = Number(req.params.id);
+			const deleted = userRepo.delete(id);
+			if (!deleted)
+				return reply.sendError({statusCode: 404, message: 'User not found'});
+			reply.code(204).send();
+		}
+	);
 }
