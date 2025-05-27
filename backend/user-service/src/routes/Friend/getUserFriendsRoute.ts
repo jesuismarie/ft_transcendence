f24418b27df5e2a4a5f3181e359e17543e1abe10
Karen Danielyan <karendanielyan66@gmail.com>
2025-05-28 01:40:35 +0200

    REFACTOR: friend routes

--- backend/user-service/src/routes/Friend/getUserFriendsRoute.ts
+++ backend/user-service/src/routes/Friend/getUserFriendsRoute.ts
@@ -1,16 +1,32 @@
 import { FastifyInstance, FastifyReply } from "fastify";
-import { listFriendsQuery } from "../../schemas/friendSchemas";
+import { listFriendsQuery, listFriendsResponseSchema } from "../../schemas/friendSchemas";
+import { UserRepo } from "../../repositories/userRepo";
+import { FriendRepo } from "../../repositories/friendRepo";
+import errorSchema from "../../schemas/errorSchema";
+import { UserTypes } from "@ft-transcendence/api-types";
 
-export default async function getUserFriendsRoute(app: FastifyInstance, userRepo: any, friendRepo: any) {
-	app.get('/friends', { schema: { querystring: listFriendsQuery } }, async (req, reply: FastifyReply) => {
-		const { userId } = req.query as { userId: number };
+export default async function getUserFriendsRoute(app: FastifyInstance, userRepo: UserRepo, friendRepo: FriendRepo) {
+	app.get<{Params: {userId: string}}>(
+		'/friends/:userId',
+		{
+			schema: {
+				querystring: listFriendsQuery,
+				response: {
+					'2xx': listFriendsResponseSchema,
+					'4xx': errorSchema
+				}
+			}
+		},
+		async (req, reply: FastifyReply) => {
+			const userId = Number(req.params.userId);
+			const { offset = 0, limit = 20, q } = req.query as UserTypes.PaginationQuery;
 		
-		const userExists = userRepo.findById(userId);
-		if (!userExists) {
-			return reply.sendError({ statusCode: 404, message: 'User not found' });
+			const userExists = userRepo.findById(userId);
+			if (!userExists) {
+				return reply.sendError({ statusCode: 404, message: 'User not found' });
+			}
+			const friends = friendRepo.list(userId, { offset, limit, q });
+			reply.send({total: friends.length, friends: friends});
 		}
-		
-		const friends = friendRepo.getFriends(userId);
-		reply.send(friends);
-	});
+	);
 }
