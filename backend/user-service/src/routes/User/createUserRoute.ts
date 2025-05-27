0cf81d5edd046cf478f508b65ac0abfc6fb08aaf
Karen Danielyan <karendanielyan66@gmail.com>
2025-05-27 21:35:08 +0200

    FIX: align createUserRoute with the new userRepo

--- backend/user-service/src/routes/User/createUserRoute.ts
+++ backend/user-service/src/routes/User/createUserRoute.ts
@@ -1,14 +1,42 @@
 import { FastifyInstance, FastifyReply } from "fastify";
 import { UserRepo } from "../../repositories/userRepo";
-import { createUserSchema } from "../../schemas/userSchemas";
+import {createUserResponseSchema, createUserSchema} from "../../schemas/userSchemas";
+import { errorSchema } from "../../schemas/errorSchema";
+import { UserTypes } from "@ft-transcendence/api-types";
 import argon2 from "argon2";
 
 export default async function createUserRoute(app: FastifyInstance, userRepo: UserRepo) {
-	app.post('/users', {schema: {body: createUserSchema}}, async (req, reply: FastifyReply) => {
-		const {email, password, displayName} = req.body as any;
-		if (userRepo.findByEmail(email)) return reply.sendError({statusCode: 409, message: 'Email already exists'});
-		const hash = await argon2.hash(password);
-		const user = userRepo.create(email, displayName, hash);
-		reply.code(201).send(user);
-	});
+	// @ts-ignore
+	app.post<{ Body: UserTypes.CreateUserRequest; Reply: UserTypes.CreateUserResponse }>(
+		'/users',
+		{
+			schema: {
+				body: createUserSchema,
+				response:
+					{
+						201: createUserResponseSchema,
+						409: errorSchema,
+						500: errorSchema
+					}
+			}
+		},
+		async (req, reply: FastifyReply) => {
+			const {email, password, username, authProvider, providerSub} = req.body;
+			if (userRepo.findByEmail(email))
+				return reply.sendError({statusCode: 409, code: 'EMAIL_EXISTS', message: 'Email already exists'});
+			if (userRepo.findByUsername(username))
+				return reply.sendError({statusCode: 409, code: 'USERNAME_EXISTS', message: 'Username already exists'});
+			const hash = await argon2.hash(password);
+			const user = userRepo.create(email, username, hash, authProvider, providerSub);
+			if (!user) {
+				return reply.sendError({statusCode: 500, message: 'Failed to create user'});
+			}
+			const replyUser: UserTypes.CreateUserResponse = {
+				id: user.id,
+				email: user.email,
+				username: user.username
+			}
+			reply.code(201).send(replyUser);
+		}
+	);
 }
