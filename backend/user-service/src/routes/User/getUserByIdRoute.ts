9e3d993ea40b6181f5c477e5da638aa5ac1069ed
Karen Danielyan <karendanielyan66@gmail.com>
2025-05-27 22:34:45 +0200

    REFACTOR: getUserById

--- backend/user-service/src/routes/User/getUserByIdRoute.ts
+++ backend/user-service/src/routes/User/getUserByIdRoute.ts
@@ -1,12 +1,17 @@
 import { FastifyInstance, FastifyReply } from "fastify";
 import { UserRepo } from "../../repositories/userRepo";
+import { UserTypes } from "@ft-transcendence/api-types";
 
 export default async function getUserByIdRoute(app: FastifyInstance, userRepo: UserRepo) {
-	app.get('/users/:id', async (req, reply: FastifyReply) => {
+	app.get<{Reply: UserTypes.UserView}>(
+		'/users/:id',
+		async (req, reply: FastifyReply) => {
 		const id = Number((req.params as any).id);
 		const user = userRepo.findById(id);
 		if (!user)
 			return reply.sendError({statusCode: 404, message: 'User not found'});
-		return user;
+		const view = userRepo.toView(user);
+		// TODO: Ask win/loss from game service
+		return view;
 	});
 }
