e95672be1d41276bc77e59ee03af12bf115e139d
Karen Danielyan <karendanielyan66@gmail.com>
2025-05-28 22:03:29 +0200

    FEAT: untested live session tracking plugin via websockets

--- backend/user-service/src/index.ts
+++ backend/user-service/src/index.ts
@@ -3,8 +3,10 @@ import fastifyStatic from '@fastify/static';
 import multipart from '@fastify/multipart';
 import fastifyRateLimit from "@fastify/rate-limit";
 import fastifyHelmet from "@fastify/helmet";
+import websocket from '@fastify/websocket';
 import dotenv from 'dotenv';
 import path from 'path';
+import fs from 'fs';
 
 
 dotenv.config();
@@ -13,14 +15,20 @@ dotenv.config();
 import dbPlugin from './plugins/db';
 import errorEnvelope from "./plugins/errorEnvelope";
 import gameServiceClientPlugin from './plugins/gameServiceClient';
+import liveSessionManagementPlugin from './plugins/live-session-management/plugin';
 
 // Routes
 import routes from './routes/routes';
 
 const app = Fastify({ logger: true });
 
+// make sure public directory exists
+const publicDir = path.join(process.cwd(), 'public');
+if (!fs.existsSync(publicDir)) {
+	fs.mkdirSync(publicDir, {recursive: true});
+}
 app.register(fastifyStatic, {
-	root: path.join(process.cwd(), 'public'),
+	root: publicDir,
 	prefix: '/static/',
 });
 app.register(fastifyHelmet, {
@@ -31,10 +39,16 @@ app.register(fastifyRateLimit, {
 	timeWindow: '1 hour'
 });
 app.register(multipart, { limits: {fileSize: 1_000_000} });
+app.register(websocket, {
+	options: {
+		maxPayload: 1048576, // 1 MB
+	},
+});
 
 app.register(errorEnvelope);
 app.register(dbPlugin);
 app.register(gameServiceClientPlugin);
+app.register(liveSessionManagementPlugin);
 
 app.register(routes);
 
