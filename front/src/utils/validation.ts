9d350b8cb0ac1cb7d35618265dced63f56f1652b
Gevorg Hovhannisyan <gevorg.hovhannisyan.outer@gmail.com>
2025-05-29 21:19:32 +0400

    [ 42Yerevan:make ] Add PersistenceService for online/offline status backend.

--- front/src/utils/validation.ts
+++ front/src/utils/validation.ts
@@ -20,3 +20,8 @@ export function isValidPassword(password: string): boolean {
 export function isValidAvatar(dataUrl: string): boolean {
 	return /^data:image\/(jpe?g|png|gif|webp);base64,/.test(dataUrl);
 }
+
+
+export function isValidTwoFACode(code: string): boolean {
+	return /^\d{6}$/.test(code);
+}
